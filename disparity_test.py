c0a24e4c0974b252f21ed464c204781930b067f8
chengbin <sbkim0407@gmail.com>
2018-07-16 20:39:15 +0900

    revise disparity_test.py

--- disparity_test.py
+++ disparity_test.py
@@ -1,7 +1,24 @@
 import numpy as np
 import cv2
+import argparse
+from datetime import datetime
 
 from sgbm import SGBM
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--video_record', dest='video_record', action='store_true', default=False,
+                    help='record raw video or not')
+args = parser.parse_args()
+
+
+class RecordVideo(object):
+    def __init__(self):
+        self.video_name = datetime.now().strftime("%Y%m%d-%H%M") + '.avi'
+        width, height = 640, 480
+
+        # define the codec and create VideoWriter object
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        self.output = cv2.VideoWriter(self.video_name, fourcc, 20.0, (width*2, height))
 
 
 def main():
@@ -9,6 +26,10 @@ def main():
 
     left_cap = cv2.VideoCapture(0)   # 0: left camera
     right_cap = cv2.VideoCapture(1)  # 1: right camera
+
+    video_writer = None
+    if args.video_record is True:
+        video_writer = RecordVideo()
 
     while True:
         # Capture frame-by-frame
@@ -18,10 +39,7 @@ def main():
         if not left_ret and not right_ret:
             print('can not read frame from one of the camera')
 
-        # left_img = left_img > 127.5
-        # right_img = right_img > 127.5
-
-        show_stereo(left_img, right_img)
+        show_stereo(left_img, right_img, video_writer)
         show_disparity(stereo, left_img, right_img)
 
         if cv2.waitKey(1) & 0xFF == 27:
@@ -30,16 +48,15 @@ def main():
     # When everyting done, release the capture
     left_cap.release()
     right_cap.release()
+    if args.video_record is True:
+        video_writer.output.release()
+
     cv2.destroyAllWindows()
 
 
-def show_stereo(left_img, right_img):
+def show_stereo(left_img, right_img, video_writer=None):
     cv2.namedWindow('Stereo')
     cv2.moveWindow('Stereo', 0, 0)
-
-    # # thresholding
-    # left_img = (left_img > 127.5).astype(np.uint8) * 255
-    # right_img = (right_img > 127.5).astype(np.uint8) * 255
 
     # Combine two imgs, height = 480, width = 640
     h, w, ch = left_img.shape
@@ -48,6 +65,9 @@ def show_stereo(left_img, right_img):
 
     # Display the input frame
     cv2.imshow('Stereo', img)
+
+    if args.video_record is True:
+        video_writer.output.write(img)
 
 
 def show_disparity(stereo, left_img, right_img):
